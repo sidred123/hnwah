45763202744b2b991d7715e1392fbb8a6c516e4e
Sid N <snallu@amplify.com>
2015-03-14 07:22:02 -0400

    Display one post and add the twitter bootstrap package.

--- hnwah.js
+++ hnwah.js
@@ -1,23 +1,33 @@
-if (Meteor.isServer) {
-    Meteor.startup(function () {
-        // 8980047 - Feb 2015
-        var postContent = httpGet("https://hacker-news.firebaseio.com/v0/item/8980047.json");
-        var comments = JSON.parse(postContent.content)["kids"];
-        Meteor.log.debug(comments);
-        _.each(comments, function (value, key) {
-            var commentContent = httpGet("https://hacker-news.firebaseio.com/v0/item/" + value + ".json");
-            Meteor.log.debug(commentContent)
-        });
-    });
-}
+var httpGet = Meteor.wrapAsync(Meteor.http.get);
 
-var httpGet = function (url) {
-    var future = new Future();
-    Meteor.http.get(url, function(error, result) {
-        if (!error) {
-            return future.return(result);
+if (Meteor.isServer) {
+    Meteor.methods({
+        "posts": function () {
+            // 8980047 - Feb 2015
+            var postContent = httpGet("https://hacker-news.firebaseio.com/v0/item/8980047.json");
+            var comments = JSON.parse(postContent.content)["kids"];
+            Meteor.log.debug("number of comments: " + comments.length);
+            var commentContents = [];
+            comments = [comments[0]];
+            _.each(comments, function (value, key) {
+                var commentContent = httpGet("https://hacker-news.firebaseio.com/v0/item/" + value + ".json");
+                //Meteor.log.debug(commentContent.data.text);
+                commentContents.push(commentContent.data.text);
+            });
+            return commentContents;
         }
-        return future.return(null);
     });
-    return future.wait();
+};
+
+if (Meteor.isClient) {
+    posts = Meteor.call("posts", function (error, data) {
+        console.log("posts returned");
+        Session.set("posts", data);
+    });
+    Template.postsTable.helpers({
+        getPosts: function () {
+            console.log("getting posts");
+            return Session.get("posts");
+        }
+    });
 }
